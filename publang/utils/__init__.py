83af62fc8d5b3c169f6f5f30c94a607671f6dd3b
Alejandro de la Vega <aleph4@gmail.com>
2024-03-26 17:44:44 -0500

    Fix all

--- publang/utils/__init__.py
+++ publang/utils/__init__.py
@@ -2,5 +2,7 @@ from .string import format_string_with_variables
 from .oai import get_openai_chatcompletion_response, get_openai_embedding_response
 
 __all__ = [
-    "format_string_with_variables"
+    "format_string_with_variables",
+    "get_openai_chatcompletion_response",
+    "get_openai_embedding_response"
 ]
