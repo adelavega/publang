5a92b97a525b38e771d8ada3acd62074ce6c0197
Alejandro de la Vega <aleph4@gmail.com>
2024-04-24 17:13:01 -0500

    Simplify parallelize function'

--- publang/utils/parallelize.py
+++ publang/utils/parallelize.py
@@ -10,30 +10,32 @@ def parallelize_inputs(func):
         # Get the number of workers (pop)
         num_workers = kwargs.pop("num_workers", 1)
 
-        # If only one text is provided, run the function directly
+        # If only one, run in serial mode
         if not isinstance(inputs, list):
-            return func(inputs, *args, **kwargs)
-                
-        # If num_workers is 1, run the function directly
-        if num_workers == 1:
-            results = []
-            for i in tqdm.tqdm(inputs):
-                results.append(func(i, *args, **kwargs))
-                if output_path is not None and len(results) % 10 == 0:
-                    json.dump(results, open(output_path, "w"))
+            num_workers = 1
+            inputs = [inputs]
 
-            return results
+        if num_workers != 1:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as exc:
+                inputs = [exc.submit(func, i, *args, **kwargs) for i in inputs]
 
-        # Otherwise, parallelize the function
-        with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as exc:
-            futures = [exc.submit(func, i, *args, **kwargs) for i in inputs]
-
+        # Loop through the inputs
         results = []
-        for future in tqdm.tqdm(futures, total=len(inputs)):
-            results.append(future.result())
-            assert 0
-            # Save every 10 results
-            if output_path is not None and len(results) % 10 == 0:
+        for input in tqdm.tqdm(inputs):
+            # Serial mode
+            if num_workers == 1:
+                results.append(func(input, *args, **kwargs))
+            else:
+                results.append(input.result())
+
+            if len(results) % 10 == 0:
+                if output_path is not None:
+                    json.dump(results, open(output_path, "w"))
+        else:
+            # If only one result, delist
+            if len(results) == 1:
+                results = results[0]
+            if output_path is not None:
                 json.dump(results, open(output_path, "w"))
         return results
 
